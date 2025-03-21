3804d1a9a9d653f1d64291a415b284e55c7b8be7
AxJrTec <A01738607@tec.mx>
2025-03-21 10:49:15 -0600

    create games.py

---
+++ games.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+import json
+
+if __name__ == "__main__":
+    spark = SparkSession\
+        .builder\
+        .appName("games")\
+        .getOrCreate()
+
+    print("leyendo dataset.csv ... ")
+    path_games="dataset.csv"
+    df_games = spark.read.csv(path_games, header=True, inferSchema=True)
+    df_games.createOrReplaceTempView("games")
+
+    query='SELECT name, metacritic, released, added_status_owned AS owned, added_status_playing AS playing FROM games WHERE metacritic is NOT NULL ORDER BY metacritic DESC'
+    
+    df_games_filtered = spark.sql(query)
+    df_games_filtered.show(20)
+    results = df_games_filtered.toJSON().collect()
+    df_games.write.mode("overwrite").json("results")
+
+    with open('results/data.json', 'w') as file:
+        json.dump(results, file)
